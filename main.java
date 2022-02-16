1ff78332d2a59f897a3e1885f9d0378df04bcf12
Naeem <naeemahmedhakro.99@gmail.com>
2022-02-16 13:13:57 +0500

    initial commit

---
+++ main.java
@@ -0,0 +1,48 @@
+
+import java.util.Scanner;
+
+public class main {
+
+    static Scanner User_input = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        System.out.print("Enter interger 1: ");
+        double input1 = User_input.nextDouble();
+
+        System.out.print("Enter interger 2: ");
+        double input2 = User_input.nextDouble();
+
+        System.out.println("Enter choice to perform operation:");
+        System.out.println("1 = add\n2 = subtract\n3 = multiply\n4 = divide");
+        int input3 = User_input.nextInt();
+                
+        // creating contructors:
+        sum obj = new sum(input1,input2);
+
+        switch (input3) {
+            case 1:
+                System.out.println("Additon performed.");
+                System.out.println("The result is " + obj.add());
+                break;
+
+            case 2:
+                // feature coming
+                // System.out.println(obj2.subtract);
+                break;
+
+            case 3:
+                // feature coming
+                // System.out.println(obj3.multiply);
+                break;
+
+            case 4:
+                // feature coming
+                // System.out.println(obj4.divide);
+                break;
+
+            default:
+                System.out.println("Wrong input..!");
+                break;
+        }
+    }
+}
---
+++ sum.java
@@ -0,0 +1,15 @@
+public class sum {
+
+    double a;
+    double b;
+
+    public sum(double num1, double num2){
+        this.a = num1;
+        this.b = num2;
+    }
+
+    public double add(){
+        double ans = a + b;
+        return ans;
+    }
+}
